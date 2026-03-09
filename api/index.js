a208e30062bc21737463282ad19df42901117d29
vinzie12 <vinceyancy.cadutdut@gmail.com>
2026-03-09 14:32:08 +0800

    feat: add api server and update package scripts

---
+++ api/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+// Basic route
+app.get('/', (req, res) => {
+  res.json({ message: 'Veridian API is running!' });
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
